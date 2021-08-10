21f8e3588bd6c499c89ce49c70f9a14641642715
Sanzid <mithylamine@gmail.com>
2021-08-10 14:25:45 +0600

    made better

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const express =  require('express')
 const app = express()
+require('dotenv').config()
 const PORT = process.env.PORT
 const path = require('path')
 const Todo = require('./models/todo')
@@ -20,29 +21,42 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("static"));
-
-
+let msg_1 = '';
+let msg_2 = ''
 app.get('/', async (req,res)=>{
     const Alltodo =  await  Todo.find({})
 
     res.render('todo.ejs',
     {
         Alltodo,
-
+        msg_1: msg_1,
+        msg_2: msg_2,
     })
 
 })
+
+
+
+
 app.get('/status/:id', async (req,res) =>{
     const todo  = await Todo.findOne({_id : req.params.id})
     if(todo.status === 'Uncompleted'){
         todo.status = 'Completed'
+        msg_2 = todo.todo 
     }else{
         todo.status = 'Uncompleted'
     }
-    console.log(todo.status)
+
     await todo.save()
+    
+    if(todo.status === 'Completed'){
+       await Todo.deleteOne({_id: req.params.id})
+       return res.redirect('/')
+    }
 
     res.redirect('/')
+
+    
 })
 app.post('/addTodo', async (req,res) => {
     const todo = req.body.todo
@@ -55,6 +69,7 @@ app.post('/addTodo', async (req,res) => {
         
     })
     await newTodo.save()
+    msg_1 = newTodo.todo 
     res.redirect("/")   
 })
 
